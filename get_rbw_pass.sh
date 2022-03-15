e012066964a411dbc29fc79fbb8a755559caee74
bluetablecloth <spam2187371263218827@protonmail.com>
2022-03-14 21:59:58 -0700

    fix: bug with https:// in query string

--- get_rbw_pass.sh
+++ get_rbw_pass.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-[[ "$QUTE_URL" =~ https?:\/\/(.*\.)?([^/.]+)\.[^/.]+ ]] 
+[[ "$QUTE_URL" =~ https?:\/\/([^/]*?\.)?([^/.]+)\.[^/.]+ ]] 
 BASE_URL="${BASH_REMATCH[2]}" # "google" from "https://accounts.google.com/help"
 
 if PASS=$(rbw get "$BASE_URL" 2>&1 );
 then
     # No error getting password: copy password to clipboard and then get TOTP (or clear)
-    TOTP=$(rbw code "$BASE_URL")
+    TOTP=$(rbw code "$BASE_URL" 2>/dev/null) 
     USER="$(rbw get --full "$BASE_URL" | sed -n '2 p' | awk  '{print $2}')"
 else
     # Error getting password (multiple choices): parse error and select entry with dmenu
@@ -17,10 +17,9 @@ else
     SELECTED_ENTRY=$(echo "${BASH_REMATCH[1]}" | xargs) # "name1" in " name1@google.com
 
     PASS=$(rbw get "$BASE_URL" "$SELECTED_ENTRY");
-    TOTP=$(rbw code "$BASE_URL" "$SELECTED_ENTRY");
+    TOTP=$(rbw code "$BASE_URL" "$SELECTED_ENTRY" 2>/dev/null) 
     USER="$(rbw get --full "$BASE_URL" "$SELECTED_ENTRY" | sed -n '2 p' | awk  '{print $2}')"
 fi
-
 
 javascript_escape() {
     # print the first argument in an escaped way, such that it can safely
@@ -82,5 +81,5 @@ printjs() {
 }
 echo "jseval -q $(printjs)" >> "$QUTE_FIFO"
 echo "$PASS" | xclip -selection clipboard 
-((sleep 4; echo "$TOTP" |  xclip -selection clipboard) &) 2>/dev/null
+((sleep 4; echo "$TOTP" |  xclip -selection clipboard) &) 
 
